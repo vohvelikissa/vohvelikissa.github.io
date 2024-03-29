3bd07f644e8e66be956816e780838e0a66d7b11d
Pessi <--add>
2024-03-29 08:39:11 +0200

    ok

---
+++ installthatbashiewashieuwuovolactovegebullshit.sh
@@ -0,0 +1,6 @@
+cd
+mkdir projects
+cd projects
+git clone git@github.com:vohvelikissa/bash_aliases
+. ~/projects/bash_aliases/.bash_aliases
+installmyaliasespls
--- old/README.md
+++
@@ -1,3 +0,0 @@
-# vohvelikissa.github.io
-
-This is atleast supposed to be deployed to Pages
