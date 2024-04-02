dd6c3d111e84a713c1d619b2c19c83024716782e
Pessi <--add>
2024-04-02 22:44:12 +0300

    ok

--- installthatbashiewashieuwuovolactovegebullshit.sh
+++ installthatbashiewashieuwuovolactovegebullshit.sh
@@ -1,6 +1,6 @@
-cd
-mkdir projects
-cd projects
-git clone git@github.com:vohvelikissa/bash_aliases
-. ~/projects/bash_aliases/.bash_aliases
-installmyaliasespls
+cd;
+mkdir projects;
+cd projects;
+git clone git@github.com:vohvelikissa/bash_aliases;
+. ~/projects/bash_aliases/.bash_aliases;
+installmyaliasespls;
